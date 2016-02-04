31bac213dc92bd888b0dc03c1eb54ff12345ea06
Daniel Pope <danielthepope@users.noreply.github.com>
2016-02-04 00:44:34 +0000

    Added super secret Mashape key check

--- server.js
+++ server.js
@@ -30,8 +30,12 @@ app.get('/:anagram(\\w+)', function(request, response) {
 app.get('/api/solve/:anagram(\\w+)', function(request, response) {
 	var anagram = request.params.anagram;
 	var variance = parseInt(request.query.variance) || 0;
+	console.log(request.headers);
+	var secret = request.headers['x-mashape-proxy-secret'];
+	if (!secret) return response.status(401).send('Header X-Mashape-Proxy-Secret is not present');
+	if (secret !== config.mashape_secret) return response.status(401).send('Invalid X-Mashape-Proxy-Secret');
 	if (variance === -1) variance = undefined;
-	response.send(susie.solve(anagram, variance));
+	return response.send(susie.solve(anagram, variance));
 })
 
 app.use(express.static('public'));
