7dddb8b04ca8b9a5bcca581a43cd54bcd13ee38a
Daniel Pope <danielthepope@users.noreply.github.com>
2016-10-09 18:19:23 +0100

    Removed hard-coded port number

--- test/features/support/world.js
+++ test/features/support/world.js
@@ -1,12 +1,13 @@
 var zombie = require('zombie');
 var HTML5  = require('html5');
 var server = require('../../../server');
+var config = require('../../../countdownconfig.js');
 
 var World = module.exports = function(){
   this.browser = new zombie({runScripts:true, debug:false, htmlParser: HTML5});
 
   this.page = function(path){
-   return "http://localhost:3000" + path
+   return "http://localhost:" + config.port + path
   };
 
   this.visit = function(path, callback){
