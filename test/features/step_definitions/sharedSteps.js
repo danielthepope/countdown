830a2795f11c21c8907ed8c43d84acdb6e399800
Dan Pope <danielthepope@users.noreply.github.com>
2018-08-19 01:09:25 +0100

    Add more tests

--- test/features/step_definitions/sharedSteps.js
+++ test/features/step_definitions/sharedSteps.js
@@ -7,8 +7,43 @@ var sharedSteps = module.exports = function(){
     this.visit('/', next);
   });
 
+  this.Given(/^I visit "([^"]*)"$/, function (page, next) {
+    this.visit(page, next);
+  });
+
+  this.Given(/^I replace the anagram with "([^"]*)"$/, function (text, next) {
+    this.browser.fill('#board', text);
+    next();
+  });
+
+  this.When(/^I press the "([^"]*)" button$/, function (buttonText, next) {
+    this.browser.pressButton(buttonText, next);
+  });
+
   this.Then(/^I should see "([^"]*)"$/, function(text, next) {
     expect(this.browser.text('body')).to.contain(text);
+    next();
+  });
+
+  this.Then(/^I should see (\d+) letters in the anagram box$/, function (letterCount, next) {
+    this.originalBoardValue = this.browser.query('#board').value;
+    expect(this.originalBoardValue).to.have.lengthOf(letterCount);
+    next();
+  });
+
+  this.Then(/^I should see some answers$/, function (next) {
+    expect(this.browser.evaluate("document.querySelectorAll('p.result').length")).to.be.greaterThan(0);
+    next();
+  });
+
+  this.Then(/^I should not see any answers$/, function (next) {
+    expect(this.browser.evaluate("document.querySelectorAll('p.result').length")).to.equal(0);
+    next();
+  });
+
+  this.Then(/^the anagram box should have different letters$/, function (next) {
+    var newBoardValue = this.browser.query('#board').value;
+    expect(newBoardValue).not.to.equal(this.originalBoardValue);
     next();
   });
 }
--- test/features/web.feature
+++ test/features/web.feature
@@ -7,3 +7,31 @@ Feature: Web interface tests
   Scenario: Visiting the home page
     Given I am on the home page
     Then I should see "Countdown"
+
+  Scenario: Solve the anagram
+    Given I am on the home page
+    Then I should see 9 letters in the anagram box
+    When I press the "Solve" button
+    Then I should see some answers
+
+  Scenario: Enter own anagram
+    Given I am on the home page
+    And I replace the anagram with "tnetennba"
+    When I press the "Solve" button
+    Then I should see "TENANT"
+
+  Scenario: Generate a new anagram
+    Given I am on the home page
+    Then I should see 9 letters in the anagram box
+    When I press the "New letters" button
+    Then the anagram box should have different letters
+
+  Scenario: Navigate to specific anagram
+    Given I visit "/tnetennba"
+    Then I should see "TENANT"
+
+  Scenario: New letters should clear previous answers
+    Given I visit "/tnetennba"
+    Then I should see "TENANT"
+    When I press the "New letters" button
+    Then I should not see any answers
